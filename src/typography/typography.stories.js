4ac146ed77324dead4efd141214bf273deaaf1ef
Joel Denning <joeldenning@gmail.com>
2019-08-02 15:14:34 -0600

    Adding documentation for typography

---
+++ src/typography/typography.stories.js
@@ -0,0 +1,14 @@
+import { storiesOf } from "@storybook/html/dist/client/preview";
+import { htmlStory } from "../story-helpers";
+import ejs from "ejs";
+import html from "./typography.stories.html";
+import { text } from "@storybook/addon-knobs";
+import "./typography.css";
+
+storiesOf("OpenMRS Styleguide", module).add("Typography & Font", () => {
+  const boldText = text("Bold text", "Bolded text");
+  const italicText = text("Italic text", "Italic text");
+  const underlinedText = text("Underlined text", "Underlined text");
+
+  return htmlStory(ejs.render(html, { boldText, italicText, underlinedText }));
+});
