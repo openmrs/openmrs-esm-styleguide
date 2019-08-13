28af86a41992bd55e77c4d8de0f56f02e7811de2
Joel Denning <joeldenning@gmail.com>
2019-08-13 08:11:12 -0600

    Fixing public path issues (#16)

--- src/openmrs-esm-styleguide.js
+++ src/openmrs-esm-styleguide.js
@@ -1,7 +1,12 @@
-import "./set-public-path.js";
-import "./canvas/canvas.css";
-import "./colors/colors.css";
-import "./typography/typography.css";
-import "./spacing/spacing.css";
-import "./cards/cards.css";
-import "./button/button.css";
+import publicPathPromise from "./set-public-path.js";
+
+publicPathPromise.then(() =>
+  Promise.all([
+    import("./canvas/canvas.css"),
+    import("./colors/colors.css"),
+    import("./typography/typography.css"),
+    import("./spacing/spacing.css"),
+    import("./cards/cards.css"),
+    import("./button/button.css")
+  ])
+);
