b68dcb3c7cfe7dede6fea1ad7738b6057d4ef3db
Fatma Ali <alifatma2019@gmail.com>
2019-08-13 17:04:44 +0300

    MF-20: Build new buttons (#12)
    * Added buttons
    
    * Review Changes
    
    * Review Changes

--- src/button/button.stories.js
+++ src/button/button.stories.js
@@ -2,22 +2,58 @@ import { storiesOf } from "@storybook/html";
 import { htmlStory } from "../story-helpers";
 import ejs from "ejs";
 import html from "./button.stories.html";
-import { radios, boolean } from "@storybook/addon-knobs";
+import { radios, boolean, select, button } from "@storybook/addon-knobs";
 import "./button.css";
 
 storiesOf("OpenMRS Styleguide", module).add("Buttons", () => {
-  const styleType = radios(
-    "Button style type",
+  const buttonType = select(
+    "Type",
     {
-      Filled: "omrs-filled",
-      Outlined: "omrs-outlined",
-      Link: "omrs-link",
-      "Unstyled (for accessibility)": "omrs-unstyled"
+      Unstyled: "unstyled",
+      Filled: "filled",
+      Outlined: "outlined",
+      Text: "text"
     },
-    "omrs-filled"
+    "filled"
+  );
+
+  const styleType = radios(
+    "Style type",
+    {
+      Action: "action",
+      Neutral: "neutral",
+      Destructive: "destructive",
+      Disabled: "disabled"
+    },
+    "action"
   );
 
   const isRounded = boolean("Rounded", false);
 
-  return htmlStory(ejs.render(html, { styleType, isRounded }));
+  const isLarge = boolean("Large", false);
+
+  const determineStyle = (buttonType, styleType, isRounded, isLarge) => {
+    let styles = `omrs-btn`;
+    if (buttonType === "unstyled") {
+      return `omrs-unstyled`;
+    }
+    styles = `${styles} omrs-${buttonType}-${styleType}`;
+    if (!isRounded && isLarge) {
+      return `${styles} omrs-btn-lg`;
+    } else if (isRounded && !isLarge) {
+      return `${styles} omrs-rounded`;
+    } else if (isRounded && isLarge) {
+      return `${styles} omrs-rounded-lg`;
+    }
+    return styles;
+  };
+  return htmlStory(
+    ejs.render(html, {
+      determineStyle,
+      buttonType,
+      styleType,
+      isRounded,
+      isLarge
+    })
+  );
 });
