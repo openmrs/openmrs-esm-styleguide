bd6768391aef972cfeb7c878552e629136a43d17
Jonathan Dick <jdick@ampath.or.ke>
2019-08-13 19:59:31 -0700

    MF-16: Changed name of title1 css to omrs-type-title-1 (#17)
    * Changed name of title1 css to omrs-type-title-1
    
    * Added title2-5 styles.
    
    * Updated font-weight on title4 and title5 to 500 instead of bold.

--- src/typography/typography.stories.js
+++ src/typography/typography.stories.js
@@ -10,8 +10,21 @@ storiesOf("OpenMRS Styleguide", module).add("Typography & Font", () => {
   const italicText = text("Italic text", "Italic text");
   const underlinedText = text("Underlined text", "Underlined text");
   const title1Text = text("Title 1 text", "Title 1 text");
+  const title2Text = text("Title 2 text", "Title 2 text");
+  const title3Text = text("Title 3 text", "Title 3 text");
+  const title4Text = text("Title 4 text", "Title 4 text");
+  const title5Text = text("Title 5 text", "Title 5 text");
 
   return htmlStory(
-    ejs.render(html, { boldText, italicText, underlinedText, title1Text })
+    ejs.render(html, {
+      boldText,
+      italicText,
+      underlinedText,
+      title1Text,
+      title2Text,
+      title3Text,
+      title4Text,
+      title5Text
+    })
   );
 });
