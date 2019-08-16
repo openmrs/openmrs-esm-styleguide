1ae63a98406abd0e0b3abef65061cc43ef48259b
Jonathan Dick <jdick@ampath.or.ke>
2019-08-16 09:48:08 -0700

    Add icons to support search feature (#22)
    * Add icons to support search feature
    
    * responding to code review
    
    * Removing default size props from svgs

--- src/icons/icons.stories.js
+++ src/icons/icons.stories.js
@@ -6,15 +6,22 @@ import { select } from "@storybook/addon-knobs";
 import "./icons.js";
 
 storiesOf("OpenMRS Styleguide", module).add("Icons", () => {
-  const iconChoice = select(
-    "Icon",
-    {
-      Visibility: "visibility"
-    },
-    "visibility"
-  );
+  const icons = {
+    Visibility: "visibility",
+    Menu: "menu",
+    Search: "search",
+    "Arrow back": "arrow-back",
+    "Arrow downward": "arrow-downward",
+    "Arrow upward": "arrow-upward",
+    "Chevron left": "chevron-left",
+    "Chevron right": "chevron-right",
+    "Supervised user circle": "supervised-user-circle"
+  };
+  const iconNames = Object.values(icons);
+
+  const iconChoice = select("Icon", icons, "visibility");
 
   const href = `#omrs-icon-${iconChoice}`;
 
-  return htmlStory(ejs.render(html, { href }));
+  return htmlStory(ejs.render(html, { href, iconNames }));
 });
