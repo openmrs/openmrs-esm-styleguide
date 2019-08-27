71819706bd93fe5959ce5027e3342895d75fe096
Joel Denning <joeldenning@gmail.com>
2019-08-26 23:28:39 -0600

    Implementing omrs-link. (#28)
    * Implementing omrs-link.
    
    * Self review
    
    * Switching links to display flex
    
    * Switching to inline flex
    
    * Self review

--- src/styleguide-entry.js
+++ src/styleguide-entry.js
@@ -4,7 +4,7 @@ import "./colors/colors.css";
 import "./typography/typography.css";
 import "./spacing/spacing.css";
 import "./cards/cards.css";
-import "./button/button.css";
+import "./buttons-and-links/buttons-and-links.css";
 import "./logo/logo.js";
 import "./icons/icons.js";
 import "./inputs/input.css";
