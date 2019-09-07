1e1372d12799c1c71a798875536683117e09feb5
Joel Denning <joeldenning@gmail.com>
2019-09-06 23:13:34 -0600

    Adding introduction story that explains things. (#33)

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,17 +3,22 @@ import { withA11y } from "@storybook/addon-a11y";
 import hljs from "highlight.js/lib/highlight";
 import xmlLanguage from "highlight.js/lib/languages/xml";
 import cssLanguage from "highlight.js/lib/languages/css";
+import jsLanguage from "highlight.js/lib/languages/javascript";
+import bashLanguage from "highlight.js/lib/languages/bash";
 import "highlight.js/styles/a11y-dark.css";
 import "highlight.js/styles/tomorrow-night.css";
 import { withKnobs } from "@storybook/addon-knobs";
 
 hljs.registerLanguage("xml", xmlLanguage);
 hljs.registerLanguage("css", cssLanguage);
+hljs.registerLanguage("js", jsLanguage);
+hljs.registerLanguage("bash", bashLanguage);
 
 addDecorator(withA11y);
 addDecorator(withKnobs);
 
 // automatically import all files ending in *.stories.js
+require("../src/intro.stories");
 const req = require.context("../src", true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
