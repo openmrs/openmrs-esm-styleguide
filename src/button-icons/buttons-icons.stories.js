3d3166b9cda9af1426c3fa0ec41cc30440177dba
Stanila Andrei <andrei133303@gmail.com>
2019-09-09 00:00:18 +0300

    button icon large

--- src/button-icons/buttons-icons.stories.js
+++ src/button-icons/buttons-icons.stories.js
@@ -1,10 +1,10 @@
 import { storiesOf } from "@storybook/html";
-import html from "./button-icons.stories.html";
-import "./button-icons.css";
+import html from "./buttons-icons.stories.html";
+import "./buttons-icons.css";
 import ejs from "ejs";
 import { text, withKnobs } from "@storybook/addon-knobs";
 import { htmlStory } from "../story-helpers";
 
-storiesOf("OpenMRS Styleguide", module).add("button icons", () => {
+storiesOf("OpenMRS Styleguide", module).add("Button icons", () => {
   return htmlStory(ejs.render(html, {}));
 });
