17d60e25f852f09970d9e68d21c7151a2886984b
Stanila Andrei <andrei133303@gmail.com>
2019-09-10 16:15:17 +0300

    Added knobs

--- src/button-icons/buttons-icons.stories.js
+++ src/button-icons/buttons-icons.stories.js
@@ -4,7 +4,33 @@ import "./button-icons.css";
 import ejs from "ejs";
 import { text, withKnobs } from "@storybook/addon-knobs";
 import { htmlStory } from "../story-helpers";
+import { select } from "@storybook/addon-knobs";
 
 storiesOf("OpenMRS Styleguide", module).add("Button icons", () => {
-  return htmlStory(ejs.render(html, {}));
+  const icons = {
+    Visibility: "visibility",
+    Menu: "menu",
+    Search: "search",
+    "Arrow back": "arrow-back",
+    "Arrow downward": "arrow-downward",
+    "Arrow upward": "arrow-upward",
+    "Chevron left": "chevron-left",
+    "Chevron right": "chevron-right",
+    "Supervised user circle": "supervised-user-circle"
+  };
+
+  const sizeOptions = {
+    Medium: "medium",
+    Large: "large"
+  };
+
+  const iconChoice = select("Icon", icons, "visibility");
+
+  const size = select("Size", sizeOptions, "medium");
+
+  const href = `#omrs-icon-${iconChoice}`;
+
+  const btnSize = `omrs-btn-icon-${size}`;
+
+  return htmlStory(ejs.render(html, { href, btnSize }));
 });
