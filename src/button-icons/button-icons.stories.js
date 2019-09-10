ae327be9bc0a5c7c6b6065c8cf4e0c8046b037ac
Stanila Andrei <andrei133303@gmail.com>
2019-09-10 16:26:24 +0300

    Renamed buttons to button

--- src/button-icons/button-icons.stories.js
+++ src/button-icons/button-icons.stories.js
@@ -1,5 +1,5 @@
 import { storiesOf } from "@storybook/html";
-import html from "./buttons-icons.stories.html";
+import html from "./button-icons.stories.html";
 import "./button-icons.css";
 import ejs from "ejs";
 import { htmlStory } from "../story-helpers";
