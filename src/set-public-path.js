7b77cbb7089b0dc716f199c8d258fbb99d8bf794
Fatma Ali <alifatma2019@gmail.com>
2019-09-11 18:03:13 +0300

    Fixed breaking changes caused by SystemJS upgrade (#36)

--- src/set-public-path.js
+++ src/set-public-path.js
@@ -1,3 +1,4 @@
-export default window.System.resolve("@openmrs/esm-styleguide").then(url => {
+export default function() {
+  const url = window.System.resolve("@openmrs/esm-styleguide");
   __webpack_public_path__ = url.slice(0, url.lastIndexOf("/") + 1);
-});
+}
