e0938ea79f0712fc8f2934ba3f8aebd1388d6e21
Donald Kibet <chelashawdonald@yahoo.com>
2019-09-16 12:26:48 +0300

    MF-68: Using systemjs-webpck-interop for public path

--- src/set-public-path.js
+++ src/set-public-path.js
@@ -1,4 +1,3 @@
-export default function() {
-  const url = window.System.resolve("@openmrs/esm-styleguide");
-  __webpack_public_path__ = url.slice(0, url.lastIndexOf("/") + 1);
-}
+import { setPublicPath } from "systemjs-webpack-interop";
+
+setPublicPath("@openmrs/esm-styleguide");
